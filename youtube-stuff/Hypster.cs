d3c27411b35d49648fa904e536085c2ec238d915
foxneZz <foxnezz@gmail.com>
2017-02-09 22:27:47 +0300

    added Hypster parsing

--- youtube-stuff/Hypster.cs
+++ youtube-stuff/Hypster.cs
@@ -17,23 +17,48 @@ namespace Hypster2Youtube
             var contents = GetWebContents(playlistLink);
             Name = GetName(contents);
             SongIds = GetSongIds(contents);
-            //System.Net.WebUtility.HtmlDecode()
         }
 
         private static List<string> GetSongIds(string contents)
         {
-            throw new NotImplementedException();
+            const string before = "items_arr[";
+
+            var result = new List<string>();
+
+            int currentIndex = 0;
+            while (true)
+            {
+                currentIndex = contents.IndexOf(before, currentIndex);
+                if (currentIndex == -1)
+                    break;
+
+                currentIndex = contents.IndexOf('\'', currentIndex) + 1;
+                result.Add(contents.Substring(currentIndex, 11));
+            }
+
+            return result;
         }
 
         private static string GetName(string contents)
         {
-            throw new NotImplementedException();
+            const string before = "0px; overflow:hidden;\">";
+            int beforeIndex = contents.IndexOf(before);
+            int startIndex = beforeIndex + before.Length;
+            int endIndex = contents.IndexOf('<', startIndex);
+
+            if (beforeIndex == -1 || endIndex == -1)
+                throw new ArgumentException("This is not a hypster playlist or this parser is outdated");
+
+            return WebUtility.HtmlDecode(contents.Substring(startIndex, endIndex - startIndex).Trim());
         }
 
         private static string GetWebContents(string link)
         {
             using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
                 return client.DownloadString(link);
+            }
         }
     }
 }
