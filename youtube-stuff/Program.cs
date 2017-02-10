c3b5faebc57a494d66405204a5db31836045fe86
foxneZz <foxnezz@gmail.com>
2017-02-10 21:57:50 +0300

    added Youtube / first working version

--- youtube-stuff/Program.cs
+++ youtube-stuff/Program.cs
@@ -1,93 +1,31 @@
 ﻿using System;
-using System.IO;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Text;
-
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Upload;
-using Google.Apis.Util.Store;
-using Google.Apis.YouTube.v3;
-using Google.Apis.YouTube.v3.Data;
-
-using Hypster2Youtube.Properties;
 
 namespace Hypster2Youtube
 {
     class Program
     {
-        private static readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
+        public static readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
         private const string playlistLink = @"http://hypster.com/playlists/userid/5049121";
 
         [STAThread]
         static void Main(string[] args)
         {
-            foreach (var song in new HypsterPlaylist(playlistLink).SongIds)
-                Console.WriteLine(song);
+            Console.WriteLine("Parsing hypster...");
+            var playlist = new HypsterPlaylist(playlistLink);
+            var youtube = new Youtube();
 
-            /*Console.WriteLine("Hypster2Youtube");
-            Console.WriteLine("==================================");
-
-            try
+            Task.Run(async () =>
             {
-                Run().Wait();
-            }
-            catch (AggregateException ex)
-            {
-                foreach (var e in ex.InnerExceptions)
-                {
-                    Console.WriteLine("Error: " + e.Message);
-                }
-            }*/
+                Console.WriteLine("Signing into youtube...");
+                await youtube.Login();
+                Console.WriteLine("Uploading the playlist...");
+                await youtube.InsertPlaylist(playlist, "hey");
+            }).GetAwaiter().GetResult();
 
-            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Done. Press any key to continue...");
             Console.ReadKey();
         }
-
-        private static async Task Run()
-        {
-            UserCredential credential;
-            using (var stream = GenerateStreamFromString(Resources.client_secrets_json))
-            {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    new[] { YouTubeService.Scope.Youtube },
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(AppName)
-                );
-            }
-
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = AppName
-            });
-
-            // Create a new, private playlist in the authorized user's channel.
-            var newPlaylist = new Playlist();
-            newPlaylist.Snippet = new PlaylistSnippet();
-            newPlaylist.Snippet.Title = "Test Playlist";
-            newPlaylist.Snippet.Description = "A playlist created with the YouTube API v3";
-            newPlaylist.Status = new PlaylistStatus();
-            newPlaylist.Status.PrivacyStatus = "private";
-            newPlaylist = await youtubeService.Playlists.Insert(newPlaylist, "snippet,status").ExecuteAsync();
-
-            // Add a video to the newly created playlist.
-            var newPlaylistItem = new PlaylistItem();
-            newPlaylistItem.Snippet = new PlaylistItemSnippet();
-            newPlaylistItem.Snippet.PlaylistId = newPlaylist.Id;
-            newPlaylistItem.Snippet.ResourceId = new ResourceId();
-            newPlaylistItem.Snippet.ResourceId.Kind = "youtube#video";
-            newPlaylistItem.Snippet.ResourceId.VideoId = "GNRMeaz6QRI";
-            newPlaylistItem = await youtubeService.PlaylistItems.Insert(newPlaylistItem, "snippet").ExecuteAsync();
-
-            Console.WriteLine("Playlist item id {0} was added to playlist id {1}.", newPlaylistItem.Id, newPlaylist.Id);
-        }
-
-        public static MemoryStream GenerateStreamFromString(string value)
-            => new MemoryStream(Encoding.UTF8.GetBytes(value));
     }
 }
---
+++ youtube-stuff/Youtube.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+using Hypster2Youtube.Properties;
+
+namespace Hypster2Youtube
+{
+    public class Youtube
+    {
+        private YouTubeService yt;
+
+        public async Task Login()
+        {
+            UserCredential credential;
+            using (var stream = GenerateStreamFromString(Resources.client_secrets_json))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { YouTubeService.Scope.Youtube },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(Program.AppName)
+                );
+            }
+
+            yt = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = Program.AppName
+            });
+        }
+
+        public async Task InsertPlaylist(HypsterPlaylist playlist, string description = "", bool makePrivate = true)
+        {
+            var newPlaylist = new Playlist();
+            newPlaylist.Snippet = new PlaylistSnippet();
+            newPlaylist.Snippet.Title = playlist.Name;
+            newPlaylist.Snippet.Description = description;
+            newPlaylist.Status = new PlaylistStatus();
+            newPlaylist.Status.PrivacyStatus = makePrivate ? "private" : "public";
+            newPlaylist = await yt.Playlists.Insert(newPlaylist, "snippet,status").ExecuteAsync();
+
+            foreach (var songId in playlist.SongIds)
+            {
+                var video = new PlaylistItem();
+                video.Snippet = new PlaylistItemSnippet();
+                video.Snippet.PlaylistId = newPlaylist.Id;
+                video.Snippet.ResourceId = new ResourceId();
+                video.Snippet.ResourceId.Kind = "youtube#video";
+                video.Snippet.ResourceId.VideoId = songId;
+                video = await yt.PlaylistItems.Insert(video, "snippet").ExecuteAsync();
+            }
+        }
+
+        private static MemoryStream GenerateStreamFromString(string value)
+            => new MemoryStream(Encoding.UTF8.GetBytes(value));
+    }
+}
